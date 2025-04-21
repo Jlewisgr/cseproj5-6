e68239c39fdfad91d02ac2fb07efaebdf5ac3671
Jlewisgr <142634996+Jlewisgr@users.noreply.github.com>
2025-04-21 16:33:42 -0700

    Create Default.aspx.designer.cs

---
+++ Default.aspx.designer.cs
@@ -0,0 +1,8 @@
+public partial class _Default
+{
+    protected global::System.Web.UI.WebControls.TextBox TaskInput;
+    protected global::System.Web.UI.WebControls.Button AddButton;
+    protected global::System.Web.UI.WebControls.GridView TasksGrid;
+    protected global::System.Web.UI.WebControls.Button btnGetTime;
+    protected global::System.Web.UI.WebControls.Label lblTime;
+}
