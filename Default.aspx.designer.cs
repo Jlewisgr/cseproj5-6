9a2eab39f610cf323d66877f605868c74bd33c9f
Jlewisgr <142634996+Jlewisgr@users.noreply.github.com>
2025-04-21 18:39:30 -0700

    Update Default.aspx.designer.cs

--- Default.aspx.designer.cs
+++ Default.aspx.designer.cs
@@ -1,5 +1,6 @@
 public partial class _Default
 {
+    //makes protected golbal variables
     protected global::System.Web.UI.WebControls.TextBox TaskInput;
     protected global::System.Web.UI.WebControls.Button AddButton;
     protected global::System.Web.UI.WebControls.GridView TasksGrid;
